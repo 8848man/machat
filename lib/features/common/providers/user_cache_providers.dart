d80423f468fe88e9c5e1fa1f94b4d9f508854253
8848man <83057037+8848man@users.noreply.github.com>
2025-06-10 15:51:12 +0900

    -- feat : member drawer

--- lib/features/common/providers/user_cache_providers.dart
+++ lib/features/common/providers/user_cache_providers.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:machat/features/common/models/user_data.dart';
+import 'package:machat/features/common/providers/chat_room_id.dart';
 
 class UserCacheNotifier extends StateNotifier<Map<String, dynamic>> {
   UserCacheNotifier() : super({});
@@ -41,7 +42,10 @@ class UserListNotifier extends StateNotifier<List<UserData>> {
 
 final roomMemberListProvider =
     StateNotifierProvider<UserListNotifier, List<UserData>>(
-  (ref) => UserListNotifier(),
+  (ref) {
+    ref.watch(chatRoomIdProvider);
+    return UserListNotifier();
+  },
 );
 
 final friendListProvider =
