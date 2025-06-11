0adbd9fda138448360445962315b59af836e90aa
김락원 <83057037+8848man@users.noreply.github.com>
2025-06-11 10:43:31 +0900

    Feat/chat room drawer (#49)
    * -- 채팅방 메뉴 버튼 추가
    
    * 채팅방 정보창 기능 구현
    
    * Revert "-- 채팅방 채팅 컨텐츠의 Stream Provider 개선 및 레포지토리 분기"
    
    This reverts commit ba0fbd8410ef3846e937a1bc3a0bad0462a27b9d.
    
    * -- feat : member drawer

--- lib/features/chat/features/end_drawer/view_models/end_drawer_view_model.dart
+++ lib/features/chat/features/end_drawer/view_models/end_drawer_view_model.dart
@@ -1,7 +1,8 @@
+import 'package:machat/features/chat/features/end_drawer/repository/end_drawer_repository.dart';
 import 'package:machat/features/common/interfaces/repository_service.dart';
 import 'package:machat/features/common/models/user_data.dart';
+import 'package:machat/features/common/providers/chat_room_id.dart';
 import 'package:machat/features/common/providers/user_cache_providers.dart';
-import 'package:machat/features/common/repositories/friend_crud_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'end_drawer_view_model.g.dart';
@@ -10,67 +11,66 @@ part 'end_drawer_view_model.g.dart';
 class EndDrawerViewModel extends _$EndDrawerViewModel {
   @override
   Future<UserListModel> build() async {
-    // 친구 리스트 State watch
     ref.watch(roomMemberListProvider);
-    return await getFriends();
+    return await getMembers();
   }
 
-  Future<void> addFriend(Map<String, dynamic> data) async {
-    final RepositoryService repository = ref.read(friendCrudRepository);
+  Future<void> addMember(Map<String, dynamic> data) async {
+    final RepositoryService repository = ref.read(memberCrudRepository);
 
     // 서버에 친구 추가 요청 → 응답으로 UserData 받는다고 가정
     await repository.create(data); // 타입: UserData
 
-    // 서버에서 받은 응답을 UserData로 변환
-    final UserData newFriend =
+    final UserData newMember =
         UserData.fromJson(data); // Map<String, dynamic> -> UserData
 
     // 캐시에 바로 추가
-    ref.read(roomMemberListProvider.notifier).addUsers(newFriend);
+    ref.read(roomMemberListProvider.notifier).addUsers(newMember);
   }
 
-  Future<UserListModel> getFriends() async {
-    List<UserData> cachedFriends = getCachedFriends();
-    if (cachedFriends.isNotEmpty) {
-      return UserListModel(friends: cachedFriends);
+  Future<UserListModel> getMembers() async {
+    List<UserData> cachedMembers = getCachedMembers();
+    if (cachedMembers.isNotEmpty) {
+      return UserListModel(friends: cachedMembers);
     }
     try {
-      final RepositoryService repository = ref.read(friendCrudRepository);
-      final List<Map<String, dynamic>> friends = await repository.readAll();
+      final String roomId = ref.read(chatRoomIdProvider);
+      final RepositoryService repository = ref.read(memberCrudRepository);
+      final List<Map<String, dynamic>> friends =
+          await repository.readAll(searchId: roomId);
 
-      final List<UserData> parsedFriends =
+      final List<UserData> parsedMembers =
           friends.map(UserData.fromJson).toList();
 
       // 빈 친구 목록인 경우, 빈 리스트를 반환합니다.
-      if (parsedFriends.isEmpty) {
+      if (parsedMembers.isEmpty) {
         return const UserListModel(friends: []);
       }
       // 친구 목록을 상태에 저장합니다.
-      cacheFriends(parsedFriends);
+      cacheMembers(parsedMembers);
 
-      return UserListModel(friends: parsedFriends);
+      return UserListModel(friends: parsedMembers);
     } catch (e) {
       rethrow;
     }
   }
-
-  Future<void> deleteFriend(String friendId) async {
-    final repository = ref.read(friendCrudRepository);
+  Future<void> deleteMember(String friendId) async {
+    final repository = ref.read(memberCrudRepository);
     await repository.delete(friendId);
 
     // 삭제 후 캐시에서도 제거
     ref.read(roomMemberListProvider.notifier).removeById(friendId);
   }
 
-  void cacheFriends(List<UserData> friends) {
+  void cacheMembers(List<UserData> friends) {
     ref.read(roomMemberListProvider.notifier).set(friends);
   }
 
-  void deleteCachedFriends() {
+  void deleteCachedMembers() {
     ref.read(roomMemberListProvider.notifier).clear();
   }
 
-  List<UserData> getCachedFriends() {
+  List<UserData> getCachedMembers() {
     return ref.read(roomMemberListProvider.notifier).list;
   }
 }
