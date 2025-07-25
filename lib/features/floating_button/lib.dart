a0dcde32b906440b35093c123287352cbfa364d5
8848man <83057037+8848man@users.noreply.github.com>
2025-07-25 14:46:42 +0900

    FAB 애니메이션 추가

--- lib/features/floating_button/lib.dart
+++ lib/features/floating_button/lib.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:machat/design_system/lib.dart';
+import 'package:machat/features/common/animated_widgets/mc_appear.dart';
 import 'package:machat/features/common/interfaces/repository_service.dart';
 import 'package:machat/features/floating_button/view_model/floating_button_view_model.dart';
 
