16c9e1c881af62c4e4f6e566961c1633cbc7f250
8848man <83057037+8848man@users.noreply.github.com>
2025-08-06 17:45:43 +0900

    영단어 등록 기능, 영단어 카드 추가

--- lib/features/study/widgets/subject_widgets.dart
+++ lib/features/study/widgets/subject_widgets.dart
@@ -41,45 +41,6 @@ class SubjectBundle extends ConsumerWidget {
     return Consumer(builder: (context, ref, child) {
       final int? vocabListLength = ref.watch(vocabularyListLengthProvider);
       final StudyViewModel notifier = ref.read(studyViewModelProvider.notifier);
-      // return ConstrainedBox(
-      //   constraints: const BoxConstraints(maxHeight: 250, maxWidth: 500),
-      //   child: Column(
-      //     children: [
-      //       Row(
-      //         children: [
-      //           buildTitleText('단어 외우기'),
-      //           const Spacer(),
-      //           buildTitleText('항목 관리',
-      //               onTap: () => notifier.goSubjectManagePage()),
-      //         ],
-      //       ),
-      //       SingleChildScrollView(
-      //         child: Column(
-      //           children: [
-      //             MCSpace().verticalHalfSpace(),
-      //             // 수업 과목 리스트 생성
-      //             ...subjectGenerator(vocabListLength),
-      //             const SizedBox(height: 8), // 간격 조절
-      //             McAppear(
-      //               delayMs: 300,
-      //               child: GestureDetector(
-      //                 onTap: () {
-      //                   final router = ref.read(goRouterProvider);
-      //                   router.pushNamed(RouterPath.addVocabulary.name);
-      //                 },
-      //                 child: buildFrameBox(
-      //                   child: const Center(
-      //                     child: Text("단어장 새로 등록하기!"),
-      //                   ),
-      //                 ),
-      //               ),
-      //             ),
-      //           ],
-      //         ),
-      //       ).expand(),
-      //     ],
-      //   ),
-      // );
       return Column(
         children: [
           Row(
